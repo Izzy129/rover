57be7baf219a2a56ce8c34aac05d007591c38c61
camh123 <cameron.h.1732@gmail.com>
2025-01-27 19:41:04 -0600

    add arduino code yay

---
+++ arduino_com/arduino_com.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // Read the incoming byte
+    String message = Serial.readString();
+    // Print the received message back to the serial monitor
+    Serial.print("Received: ");
+    Serial.println(message);
+  }
+}
