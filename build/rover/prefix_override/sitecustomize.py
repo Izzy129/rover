374d1e603c45e284c86d6c20fca2244767f8410c
camh123 <cameron.h.1732@gmail.com>
2025-01-27 19:36:38 -0600

    added node as executable

--- build/rover/prefix_override/sitecustomize.py
+++ build/rover/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/baracuda/Desktop/Rice Robotics/workspace/install/rover'
+    sys.prefix = sys.exec_prefix = '/home/cameron/Documents/robotics rover/rover/install/rover'
